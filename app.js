96242ad865ea2255007a836baa795f45af4ed0c8
loczekkrol <blazejek.biskup@gmail.com>
2022-10-20 21:52:17 +0200

    Add react typescript client app

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ const flash = require('connect-flash');
 const routes = require('./routes/index');
 
 const app = express();
+const cons = require('consolidate'); 
 
+app.engine('html', cons.swig);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(flash());
 
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const app = require('./app');
 
-app.set('port', process.env.PORT || 8888);
+app.set('port', process.env.PORT || 5000);
 
 const server = app.listen(app.get('port'), () => {
     console.log(`Listening on ${ server.address().port }`);
+});
+
+app.get('/express_backend', (req, res) => {
+    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+app.get("/api", (req, res) => {
+    res.json({ "users": ["userOne", "userTwo"]})
 });
